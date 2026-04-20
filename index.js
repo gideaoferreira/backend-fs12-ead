3374971b13cf60544e56f0ff6daa71408b7ada6c
Gideão Ferreira <gideaoferreira@hotmail.com>
2026-04-20 20:45:59 -0300

    Config do express, server e rotas

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("App NodeJs")
+import express from "express"
+import cors from "cors"
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (request, response) => {
+    return response
+            .status(201)
+            .json("APP EM EXECUÇÃO")
+})
+
+app.listen(port, (error) => {
+    if (error) {
+        console.log("Ops! Tivemos um erro ao iniciar o servidor")
+        return
+    }
+    console.log(`App is running on port: ${port}`)
+})
