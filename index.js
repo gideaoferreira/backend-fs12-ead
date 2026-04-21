fa6abeb3d1ae7ab431a7ad6e0b984b69c7a56706
Gideão Ferreira <gideaoferreira@hotmail.com>
2026-04-20 21:41:56 -0300

    Organização das controllers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express"
 import cors from "cors"
+import homeController from "./src/controllers/home-controller.js"
+import userController from "./src/controllers/user-controller.js"
 
 const app = express()
 const port = 3000
@@ -7,11 +9,8 @@ const port = 3000
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (request, response) => {
-    return response
-            .status(201)
-            .json("APP EM EXECUÇÃO")
-})
+app.get("/", homeController)
+app.get("/users", userController)
 
 app.listen(port, (error) => {
     if (error) {
