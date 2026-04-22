1597f53a69d4dce7f0dcd01d1e0acf181de42df4
Gideão Ferreira <gideaoferreira@hotmail.com>
2026-04-22 20:18:49 -0300

    Config de banco de dados com sequelize

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import routes from "./src/routes/routes.js"
+import { sequelize } from "./src/database/configuration.js"
 
 const app = express()
 const port = 3000
@@ -9,10 +10,17 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(port, (error) => {
-    if (error) {
-        console.log("Ops! Tivemos um erro ao iniciar o servidor")
-        return
-    }
-    console.log(`App is running on port: ${port}`)
-})
+sequelize.authenticate()
+    .then(() => {
+        app.listen(port, (error) => {
+            if (error) {
+                console.log("Ops! Tivemos um erro ao iniciar o servidor")
+                return
+            }
+            console.log(`App is running on port: ${port}`)
+        })
+    })
+    .catch((error) => {
+        console.log(`Erro ao tentar se conectar ao banco de dados: ${error}`)
+    })
+
